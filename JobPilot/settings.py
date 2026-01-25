80c9c8ead3c4b25310f347cd4e4b9dc0db483443
agomez <angeaxelgomez2004@gmail.com>
2026-01-26 00:08:09 +0100

    update des chemins d'accès pour le logo

--- JobPilot/settings.py
+++ JobPilot/settings.py
@@ -36,7 +36,7 @@ if not SECRET_KEY:
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG") == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['10.192.66.109', 'localhost', '127.0.0.1']
 
 
 # Application definition
