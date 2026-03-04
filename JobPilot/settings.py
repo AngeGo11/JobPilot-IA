9e1af9a478161938abca8b6b0c685bc76d62e2cc
agomez <angeaxelgomez2004@gmail.com>
2026-03-04 23:46:08 +0100

    update for login

--- JobPilot/settings.py
+++ JobPilot/settings.py
@@ -459,3 +459,6 @@ SOCIALACCOUNT_EMAIL_AUTHENTICATION_AUTO_CONNECT = True
 #Permet de passer l'écran de vérification
 SOCIALACCOUNT_EMAIL_VERIFICATION = "none"
 
+# On force Django à utiliser l'E-MAIL pour se connecter (et non le pseudo)
+ACCOUNT_AUTHENTICATION_METHOD = 'email'
+
