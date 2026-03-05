bbaaf48114a15b44d8cc088bbdb0c7836e548956
agomez <angeaxelgomez2004@gmail.com>
2026-03-05 01:31:01 +0100

    Correction du nom de domaine dans le mail de reinitialisation du mdp

--- users/apps.py
+++ users/apps.py
@@ -8,4 +8,24 @@ class UsersConfig(AppConfig):
 
     def ready(self):
         import users.signals  # noqa: F401
-        
+        self._ensure_site_domain()
+
+    def _ensure_site_domain(self):
+        """Met à jour le Site Django (SITE_ID) avec le domaine de SITE_URL pour les emails et liens."""
+        from django.conf import settings
+        from django.contrib.sites.models import Site
+        site_url = getattr(settings, "SITE_URL", "").strip()
+        if not site_url:
+            return
+        domain = site_url.replace("https://", "").replace("http://", "").rstrip("/").split("/")[0]
+        if not domain:
+            return
+        try:
+            site = Site.objects.filter(pk=settings.SITE_ID).first()
+            if site and site.domain != domain:
+                site.domain = domain
+                site.name = site.name or domain
+                site.save(update_fields=["domain", "name"])
+        except Exception:
+            pass
+
